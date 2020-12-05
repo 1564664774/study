7e51037eac0c07513cf1b793a9dc80d4d8f87ae9
1564664774 <zqh1564664774@163.com>
2020-12-05 16:19:07 +0800

    Initial commit

---
+++ commons/src/main/java/com/zqh/pojo/User.java
@@ -0,0 +1,20 @@
+package com.zqh.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author zhuangqinghui
+ * @version 1.0
+ * @date 2020/10/16 14:44
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private String name;
+
+    private Integer age;
+}
---
+++ shiro/src/main/java/com/zqh/ShiroApplication.java
@@ -0,0 +1,16 @@
+package com.zqh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhuangqinghui
+ * @version 1.0
+ * @date 2020/12/5 16:12
+ */
+@SpringBootApplication
+public class ShiroApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroApplication.class, args);
+    }
+}
