db4bf9717bc8d4107f4a3576d775a1007908a72e
庄青辉 <zqh1564664774@163.com>
2020-12-17 21:21:04 +0800

    修改

---
+++ algorithm/src/main/java/com/zqh/AlgorithmApplication.java
@@ -0,0 +1,16 @@
+package com.zqh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhuangqinghui
+ * @version 1.0
+ * @date 2020/12/6 17:44
+ */
+@SpringBootApplication
+public class AlgorithmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmApplication.class);
+    }
+}
---
+++ algorithm/src/test/java/com/zqh/MyTest.java
@@ -0,0 +1,24 @@
+package com.zqh;
+
+import org.junit.Test;
+
+/**
+ * @author zhuangqinghui
+ * @version 1.0
+ * @date 2020/12/16 21:05
+ */
+public class MyTest {
+
+    @Test
+    public void StringTest() {
+        String s = "java java helloWord";
+        String replace = s.replaceAll("java", "zqh");
+        System.out.println(replace);
+    }
+    @Test
+    public void StringTest2() {
+        String s= ",1111,";
+        String[] split = s.split(",");
+        System.out.println(split.length);
+    }
+}
