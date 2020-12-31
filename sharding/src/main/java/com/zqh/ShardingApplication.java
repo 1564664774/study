b2ffe743046f8a34424a530f90016039d40c842a
庄青辉 <zqh1564664774@163.com>
2020-12-31 10:36:35 +0800

    修改

---
+++ sharding/src/main/java/com/zqh/ShardingApplication.java
@@ -0,0 +1,16 @@
+package com.zqh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhuangqinghui
+ * @version 1.0
+ * @date 2020/12/29 13:46
+ */
+@SpringBootApplication
+public class ShardingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingApplication.class, args);
+    }
+}
